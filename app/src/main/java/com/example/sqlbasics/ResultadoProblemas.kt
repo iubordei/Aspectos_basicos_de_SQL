e988c9dbf7a37185dec25651a8578bbd65c6efaf
Iura Bordei <iubordei@unirioja.es>
2022-03-13 15:42:41 +0100

    Ejercicio 1 - Aspectos básicos de SQL

---
+++ app/src/main/java/com/example/sqlbasics/ResultadoProblemas.kt
@@ -0,0 +1,17 @@
+package com.example.sqlbasics
+
+/*
+
+    -- Problema 1
+    SELECT name FROM park WHERE park_visitors < 1000000;
+
+    -- Problema 2
+    SELECT COUNT(DISTINCT city) FROM park;
+    
+    -- Problema 3
+    SELECT SUM(park_visitors) from park WHERE CITY LIKE 'SAN FRANCISCO';
+
+    -- Problema 4
+    SELECT name, park_visitors FROM park ORDER BY park_visitors DESC LIMIT 5;
+
+ */
